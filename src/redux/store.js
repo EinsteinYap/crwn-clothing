7b2b924e9b16844346256110472927545f6955d9
EInstein Yap <einsteinyap2014@gmail.com>
2020-12-12 13:32:44 +0800

    updating our application with reselect or memoization

--- src/redux/store.js
+++ src/redux/store.js
@@ -7,4 +7,8 @@ const middlewares =[logger]
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
+//or
+// const store = createStore(rootReducer, applyMiddleware(logger));
+//also can , why use the above method, because want to modify the array [logger] to add in mother things
+
 export default store;
