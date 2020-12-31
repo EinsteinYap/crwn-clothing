2963efefaa969dca6256d3229fd232ea97964b20
EInstein Yap <einsteinyap2014@gmail.com>
2021-01-01 00:15:39 +0800

    lazy loading added

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React,{lazy,Suspense} from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 import {GlobalStyle } from './global.styles';
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CheckoutPage from './pages/checkout/checkout.component';
+// import HomePage from './pages/homepage/homepage.component';
+// import ShopPage from './pages/shop/shop.component';
+// import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+// import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
 import { checkUserSession } from './redux/user/user.actions';
-
+const HomePage = lazy(()=> import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(()=>import ('./pages/shop/shop.component'));
+const SignInAndSignUpPage = lazy(()=>import ('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
+const CheckoutPage = lazy(()=>import ('./pages/checkout/checkout.component'));
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
@@ -33,9 +36,15 @@ class App extends React.Component {
       <GlobalStyle/>
         <Header />
         <Switch>
+        <Suspense fallback={<div>loading...</div>}>
           <Route exact path='/' component={HomePage} />
+          
+         
           <Route path='/shop' component={ShopPage} />
+          
           <Route exact path='/checkout' component={CheckoutPage} />
+     
+
           <Route
             exact
             path='/signin'
@@ -47,6 +56,8 @@ class App extends React.Component {
               )
             }
           />
+        </Suspense>
+
         </Switch>
       </div>
     );
